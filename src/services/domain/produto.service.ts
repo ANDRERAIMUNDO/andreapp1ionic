c181e6db55c74686d77caefba4dedad8e4bda457
ANDRERAIMUNDO <9000andre@gmail.com>
2021-02-11 11:43:56 -0300

    infiniteScroll produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -6,22 +6,22 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ProdutoService {
-  constructor(public htttp: HttpClient) {
+  constructor(public http: HttpClient) {
   }
   findById(produto_id: string) {
-    return this.htttp.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
   }
-  findByCategoria(categoria_id: string) {
-    return this.htttp.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  findByCategoria(categoria_id: string, page: number = 0, linesPerPage: number = 24) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`);
   }
   getSmallImageFromBucket(id: string): Observable<any> {
     let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
-    return this.htttp.get(url, { responseType: 'blob' });
+    return this.http.get(url, { responseType: 'blob' });
 
   }
   getImageFromBucket(id: string): Observable<any> {
     let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
-    return this.htttp.get(url, { responseType: 'blob' });
+    return this.http.get(url, { responseType: 'blob' });
 
   }
 }
